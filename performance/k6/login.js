2d5926094914cbc8ab53347a868b43c87e90de6c
anup <wasakeanup88@gmail.com>
2026-08-25 13:26:17 +0530

    Adjust k6 login performance threshold

--- performance/k6/login.js
+++ performance/k6/login.js
@@ -6,7 +6,7 @@ export const options = {
     duration: '30s',
     thresholds: {
         http_req_failed: ['rate<0.05'],
-        http_req_duration: ['p(95)<2000'],
+        http_req_duration: ['p(95)<5000'],
     },
 };
 
